3cf3b0060202d69e40e67cef80d6772c604dfe7e
gwongilnam <nimpa3201@naver.com>
2025-03-17 16:35:46 +0900

    feat[#40] : shipping-service 다시 구현

---
+++ shipping-service/src/test/java/com/sparta/shippingservice/ShippingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sparta.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
