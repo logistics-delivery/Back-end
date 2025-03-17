eb44ffc2dcd83de25d742a10d363c42e4163ee7d
sohyun119 <sh-119@naver.com>
2025-03-18 01:08:49 +0900

    ✨ feat : hub list 조회 및 특정 hub 조회

---
+++ hub-service/src/main/java/com/sparta/hubservice/application/dto/HubRequestDto.java
@@ -0,0 +1,22 @@
+package com.sparta.hubservice.application.dto;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HubRequestDto {
+
+    private String name;
+    private String address;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+
+}
