785a617d3dd8cb7b07e28d4b15711bae1b92b644
jiyun <jiyyy0720@naver.com>
2025-03-19 02:22:57 +0900

    ✨ feature [#57] : Company - Read 추가

--- company-service/src/main/java/com/sparta/companyservice/application/service/CompanyService.java
+++ company-service/src/main/java/com/sparta/companyservice/application/service/CompanyService.java
@@ -7,6 +7,9 @@ import com.sparta.companyservice.presentation.request.CompanyCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
@@ -21,6 +24,19 @@ public class CompanyService {
                 request.address(),
                 userId
         );
+        // presentation에서 받은 요청을 파싱해서 domainService에 넘기고 처리 결과를 DTO로 만들어서 다시 controller로 반환
+        return CompanyDto.fromEntity(company);
+    }
+
+    public List<CompanyDto> getAllCompanies() {
+        return companyDomainService.findAll()
+                .stream()
+                .map(CompanyDto::fromEntity)
+                .toList();
+    }
+
+    public CompanyDto getCompanyById(UUID id) {
+        Company company = companyDomainService.findById(id);
         return CompanyDto.fromEntity(company);
     }
 }
