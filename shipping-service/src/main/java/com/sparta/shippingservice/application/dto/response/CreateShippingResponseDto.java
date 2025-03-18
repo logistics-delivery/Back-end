ca45e0ec77512bc654c7b27ddb99365f3636903d
gwongilnam <nimpa3201@naver.com>
2025-03-19 00:29:28 +0900

    ♻️ refactor[#33] : shipping-service Create responseDto 수정

--- shipping-service/src/main/java/com/sparta/shippingservice/application/dto/response/CreateShippingResponseDto.java
+++ shipping-service/src/main/java/com/sparta/shippingservice/application/dto/response/CreateShippingResponseDto.java
@@ -1,14 +1,24 @@
 package com.sparta.shippingservice.application.dto.response;
 
 import com.sparta.shippingservice.domain.model.Shipping;
+import com.sparta.shippingservice.domain.model.ShippingStatus;
 
 import java.util.UUID;
 
 public record CreateShippingResponseDto(
-    UUID shippingId
+    UUID shippingId,
+    String shippingAddress,
+    String receiverName,
+    UUID shippingManagerId,
+    ShippingStatus status
 ){
     public static CreateShippingResponseDto send (Shipping shipping) {
-        return new CreateShippingResponseDto(shipping.getId());
+        return new CreateShippingResponseDto(
+                shipping.getId(),
+                shipping.getShippingAddress(),
+                shipping.getReceiverName(),
+                shipping.getShippingManagerId(),
+                shipping.getStatus());
     }
 
 
