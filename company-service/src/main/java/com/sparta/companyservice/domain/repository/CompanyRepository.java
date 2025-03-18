9fd5c6cdd4e291d0524cca661c4efcc7e0458282
jiyun <jiyyy0720@naver.com>
2025-03-19 01:39:41 +0900

    ✨ Refactor [#57] : Repository 코드 리팩토링

--- company-service/src/main/java/com/sparta/companyservice/domain/repository/CompanyRepository.java
+++ company-service/src/main/java/com/sparta/companyservice/domain/repository/CompanyRepository.java
@@ -1,8 +1,6 @@
 package com.sparta.companyservice.domain.repository;
 
 import com.sparta.companyservice.domain.model.Company;
-import java.util.Optional;
-import java.util.UUID;
 
 public interface CompanyRepository {
     Company save(Company company);
