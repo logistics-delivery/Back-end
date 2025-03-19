1e82d2c65852fa07ae006b46d85925806551e04d
jin2304 <search2304@naver.com>
2025-03-19 17:18:27 +0900

    ✨ feat[#59]: 상품 단일 조회 Api 및 응답 Dto 추가

--- product-service/src/main/java/com/sparta/product/presentation/ProductController.java
+++ product-service/src/main/java/com/sparta/product/presentation/ProductController.java
@@ -3,9 +3,12 @@ package com.sparta.product.presentation;
 import com.sparta.product.application.service.ProductServiceImpl;
 import com.sparta.product.presentation.dto.request.CreateProductRequestDto;
 import com.sparta.product.presentation.dto.response.CreateProductResponseDto;
+import com.sparta.product.presentation.dto.response.ReadProductResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -15,11 +18,20 @@ public class ProductController {
     private final ProductServiceImpl productServiceImpl;
 
     /**
-     *  상품 생성
+     * 상품 생성
      */
     @PostMapping
     public ResponseEntity<CreateProductResponseDto> createProduct(@RequestBody CreateProductRequestDto requestDto,
                                                                   @RequestHeader(value = "X-User-Id", required = true) Long userId) {
         return ResponseEntity.ok(productServiceImpl.createProduct(requestDto, userId));
+    }
+
+
+    /**
+     * 상품 단일 조회
+     */
+    @GetMapping("/{productId}")
+    public ResponseEntity<ReadProductResponseDto> readProduct(@PathVariable UUID productId) {
+        return ResponseEntity.ok(productServiceImpl.readProduct(productId));
     }
 }
