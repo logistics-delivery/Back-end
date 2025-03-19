926cd71220ce8d1b45e244cf5f68e14cb2e52f62
gwongilnam <nimpa3201@naver.com>
2025-03-19 12:02:38 +0900

    ✨ feat[#33] : shipping-service 단건 조회 구현

--- shipping-service/src/main/java/com/sparta/shippingservice/application/dto/response/ShippingResponseDto.java
+++ shipping-service/src/main/java/com/sparta/shippingservice/application/dto/response/ShippingResponseDto.java
@@ -5,15 +5,15 @@ import com.sparta.shippingservice.domain.model.ShippingStatus;
 
 import java.util.UUID;
 
-public record CreateShippingResponseDto(
+public record ShippingResponseDto(
     UUID shippingId,
     String shippingAddress,
     String receiverName,
     UUID shippingManagerId,
     ShippingStatus status
 ){
-    public static CreateShippingResponseDto send (Shipping shipping) {
-        return new CreateShippingResponseDto(
+    public static ShippingResponseDto send (Shipping shipping) {
+        return new ShippingResponseDto(
                 shipping.getId(),
                 shipping.getShippingAddress(),
                 shipping.getReceiverName(),
