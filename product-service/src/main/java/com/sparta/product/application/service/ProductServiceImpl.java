aa86b2f8361208eff77e8a40a5ea562ef9c2904d
jin2304 <search2304@naver.com>
2025-03-19 17:22:44 +0900

    ✨ feat[#59]: 상품 단일 조회 기능 추가

--- product-service/src/main/java/com/sparta/product/application/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/sparta/product/application/service/ProductServiceImpl.java
@@ -1,12 +1,16 @@
 package com.sparta.product.application.service;
 
+import com.sparta.commonmodule.exception.ResourceNotFoundException;
 import com.sparta.product.domain.model.Product;
 import com.sparta.product.domain.repository.ProductRepository;
 import com.sparta.product.presentation.dto.request.CreateProductRequestDto;
 import com.sparta.product.presentation.dto.response.CreateProductResponseDto;
+import com.sparta.product.presentation.dto.response.ReadProductResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +21,23 @@ public class ProductServiceImpl implements ProductService {
 
 
     /**
-     *  상품 생성
+     * 상품 생성
      */
     @Override
     public CreateProductResponseDto createProduct(CreateProductRequestDto requestDto, Long userId) {
         Product product = productRepository.save(Product.createProduct(requestDto, userId));
         return CreateProductResponseDto.from(product);
+    }
+
+
+    /**
+     * 상품 단일 조회
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public ReadProductResponseDto readProduct(UUID productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("찾을 수 없는 상품 입니다."));
+        return ReadProductResponseDto.from(product);
     }
 }
--- product-service/src/main/java/com/sparta/product/domain/repository/ProductRepository.java
+++ product-service/src/main/java/com/sparta/product/domain/repository/ProductRepository.java
@@ -2,7 +2,13 @@ package com.sparta.product.domain.repository;
 
 import com.sparta.product.domain.model.Product;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProductRepository {
 
     Product save(Product product);
+
+    Optional<Product> findById(UUID productId);
+
 }
