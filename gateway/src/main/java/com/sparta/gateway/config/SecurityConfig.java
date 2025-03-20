5fcc73979fc2766056a57da32902256c04af41ac
devdaeun <ekdms008@gmail.com>
2025-03-20 16:25:07 +0900

    ♻️ refactor [#97] : gateway, user 리팩토링 - GATEWAY에서의  권한 인가처리를 위한 security filter이동 - 필터 검증 후 권한을 점검할 수 있도록 @Component 어노테이션 삭제 - jwt 토큰값 검증 후 reqeustheader에 payload내용들만 추가하여 전달되도록 수정. - user 서비스 security 의존성 삭제

---
+++ gateway/src/main/java/com/sparta/gateway/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.sparta.gateway.config;
+
+import com.sparta.gateway.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("api/v1/users/sign-in", "api/v1/users/sign-up").permitAll()
+                        .pathMatchers("/api/v1/**").permitAll() // 권한 기반 접근
+                        .anyExchange().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .build();
+    }
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(secretKey);
+    }
+
+
+}
