3f7aae19c790c64a2b3824deefcb2253d1bf8fe6
sohyun119 <sh-119@naver.com>
2025-03-20 22:39:37 +0900

    ✨ feat : 허브 간 경로 정보 삭제 api 구현

--- hub-service/src/main/java/com/sparta/hubservice/hub_route/application/dto/response/HubRouteDeleteResponse.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub_route/application/dto/response/HubRouteDeleteResponse.java
@@ -1,10 +1,8 @@
 package com.sparta.hubservice.hub_route.application.dto.response;
 
 import java.util.UUID;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 
 @Builder
