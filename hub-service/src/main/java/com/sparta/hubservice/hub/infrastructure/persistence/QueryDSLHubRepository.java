d9bd1c6751bb198e7588126d893a7761759ff8d4
devdaeun <ekdms008@gmail.com>
2025-03-21 03:50:49 +0900

    ♻️ refactor [#107] : gateway 인가 리팩토링 - gateway filter에서 우선적으로 권한을 확인하도록 구현

--- hub-service/src/main/java/com/sparta/hubservice/hub/infrastructure/persistence/QueryDSLHubRepository.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub/infrastructure/persistence/QueryDSLHubRepository.java
@@ -3,6 +3,7 @@ package com.sparta.hubservice.hub.infrastructure.persistence;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.hubservice.hub.domain.model.Hub;
+import com.sparta.hubservice.hub.domain.model.QHub;
 import com.sparta.hubservice.hub.domain.repository.HubQueryRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
