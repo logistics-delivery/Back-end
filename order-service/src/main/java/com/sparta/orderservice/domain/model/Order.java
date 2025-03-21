6ae4148bebc0c07ee81c3f1e6a2469fa92e0ec47
dydwo6018 <dydwo6018@gmail.com>
2025-03-21 18:19:07 +0900

    ✨ feat : 주문 취소 기능

--- order-service/src/main/java/com/sparta/orderservice/domain/model/Order.java
+++ order-service/src/main/java/com/sparta/orderservice/domain/model/Order.java
@@ -82,5 +82,14 @@ public class Order extends BaseEntity {
         super.delete(0L); // 사용자 인증 시스템 없으므로 임시로 0L 사용
     }
 
+    // 주문 cancel
+    public void cancel(String cancelReason) {
+        if (this.status != OrderStatus.CREATED) {
+            throw new IllegalStateException("CREATED 상태의 주문만 취소할 수 있습니다.");
+        }
+        this.status = OrderStatus.CANCELED;
+        this.cancelReason = cancelReason;
+    }
+
 }
 
