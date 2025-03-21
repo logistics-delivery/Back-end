642e339106783f9a25e4bf3a19c9ab3b83ea029a
jiyun <jiyyy0720@naver.com>
2025-03-21 14:24:17 +0900

    ✨ feat [#109] : Company 스웨거 설정

--- company-service/src/main/java/com/sparta/companyservice/presentation/controller/CompanyController.java
+++ company-service/src/main/java/com/sparta/companyservice/presentation/controller/CompanyController.java
@@ -6,6 +6,8 @@ import com.sparta.companyservice.presentation.request.CompanyCreateRequest;
 import com.sparta.companyservice.presentation.request.CompanyUpdateRequest;
 import com.sparta.companyservice.presentation.response.CompanyDeleteResponse;
 import com.sparta.companyservice.presentation.response.CompanyResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +19,13 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/companies")
 @RequiredArgsConstructor
+@Tag(name = "Company Service", description = "업체 서비스 API")
 public class CompanyController {
 
     private final CompanyService companyService;
 
     // 생성
+    @Operation(summary = "Company 등록", description = "Company 등록 API")
     @PostMapping
     public ResponseEntity<CompanyResponse> createCompany(@Valid @RequestBody CompanyCreateRequest request) {
         CompanyDto createdCompany = companyService.createCompany(request.toDto());
@@ -29,6 +33,7 @@ public class CompanyController {
     }
 
     // 전체 조회
+    @Operation(summary = "Company 전체 조회", description = "Company 전체 조회 API")
     @GetMapping
     public ResponseEntity<List<CompanyResponse>> getAllCompanies() {
         List<CompanyDto> companies = companyService.getAllCompanies();
@@ -37,6 +42,7 @@ public class CompanyController {
     }
 
     // 단일 조회
+    @Operation(summary = "Company 단건 조회", description = "Company 단건 조회 API")
     @GetMapping("/{companyId}")
     public ResponseEntity<CompanyResponse> getCompanyById(@PathVariable UUID companyId) {
         CompanyDto oneCompany = companyService.getCompanyById(companyId);
@@ -44,6 +50,7 @@ public class CompanyController {
     }
 
     // 수정
+    @Operation(summary = "Company 수정", description = "Company 수정 API")
     @PatchMapping("/{companyId}")
     public ResponseEntity<CompanyResponse> updateCompany(@PathVariable UUID companyId, @Valid @RequestBody CompanyUpdateRequest request) {
         CompanyDto updatedCompany = companyService.updateCompany(companyId, request.toDto());
@@ -51,6 +58,7 @@ public class CompanyController {
     }
 
     // 삭제
+    @Operation(summary = "Company 삭제", description = "Company 삭제 API")
     @DeleteMapping("/{companyId}")
     public ResponseEntity<CompanyDeleteResponse> deleteCompany(@PathVariable UUID companyId) {
         CompanyDeleteResponse deletedCompany = companyService.deleteCompany(companyId);
