2e080e2c2058fb7ba66a75c6f78b7a0ba6f3c74b
sohyun119 <sh-119@naver.com>
2025-03-22 22:31:36 +0900

    ✨ feat : from->to 최단 경로 생성 (path) Service 구현

--- hub-service/src/main/java/com/sparta/hubservice/hub_route/application/service/HubRouteService.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub_route/application/service/HubRouteService.java
@@ -3,12 +3,19 @@ package com.sparta.hubservice.hub_route.application.service;
 import com.sparta.commonmodule.exception.ResourceNotFoundException;
 import com.sparta.hubservice.hub.domain.model.Hub;
 import com.sparta.hubservice.hub.domain.repository.HubRepository;
-import com.sparta.hubservice.hub_route.application.dto.response.HubRouteCreateResponse;
-import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDeleteResponse;
-import com.sparta.hubservice.hub_route.application.dto.response.HubRouteResponse;
+import com.sparta.hubservice.hub_route.application.dijkstra.PathCalculate;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteCreateResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDeleteResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDetailsResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.serviceDto.PathValueDto;
 import com.sparta.hubservice.hub_route.domain.model.HubRoute;
+import com.sparta.hubservice.hub_route.domain.model.HubRouteCheckpoint;
 import com.sparta.hubservice.hub_route.domain.repository.HubRouteCheckpointRepository;
 import com.sparta.hubservice.hub_route.domain.repository.HubRouteRepository;
+import jakarta.persistence.EntityExistsException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -26,54 +33,50 @@ public class HubRouteService {
     private final HubRouteRepository hubRouteRepository;
     private final HubRepository hubRepository;
     private final HubRouteCheckpointRepository checkpointRepository;
+    private final PathCalculate pathCalculate;
 
     // 전체 허브 간 경로 목록 조회
     @Transactional(readOnly = true)
-    public Page<HubRouteResponse> getHubRoutes(Pageable pageable) {
+    public Page<HubRouteResponseDto> getHubRoutes(Pageable pageable) {
         Page<HubRoute> hubRoutes = hubRouteRepository.findAllByIsDeletedFalse(pageable)
             .orElseThrow(ResourceNotFoundException::new);
 
-        return hubRoutes.map(HubRouteResponse::new);
+        return hubRoutes.map(HubRouteResponseDto::new);
     }
 
     // 특정 경로 ID 조회
-    public HubRouteResponse getHubRoute(UUID hubRouteId) {
+    public HubRouteResponseDto getHubRoute(UUID hubRouteId) {
         HubRoute hubRoute = hubRouteRepository.findById(hubRouteId).orElseThrow(ResourceNotFoundException::new);
-        return new HubRouteResponse(hubRoute);
+        return new HubRouteResponseDto(hubRoute);
     }
 
-    // 출발 허브 -> 도착 허브 : 특정 경로 조회
+    // 출발 허브 -> 도착 허브 : 특정 경로 조회 (direct)
     @Transactional(readOnly = true)
-    public HubRouteResponse getHubRouteFromHubToHub(UUID fromHubId, UUID toHubId) {
+    public HubRouteResponseDto getDirectHubRoute(UUID fromHubId, UUID toHubId) {
 
         Optional<HubRoute> hubRoute = hubRouteRepository.findByFromHub_HubIdAndToHub_HubIdAndIsDeletedFalse(fromHubId, toHubId);
 
         if(hubRoute.isEmpty()){
             throw new ResourceNotFoundException("Hub route not found");
         }
-        return new HubRouteResponse(hubRoute.get());
+        return new HubRouteResponseDto(hubRoute.get());
     }
 
-    // 허브 간 경로 생성
+    // 허브 간 경로 생성 (direct 연결)
     @Transactional
-    public HubRouteCreateResponse createHubRoute(Long userId, UUID fromHubId, UUID toHubId) {
-        Hub fromHub = hubRepository.findById(fromHubId).get();
-        Hub toHub = hubRepository.findById(toHubId).get();
+    public HubRouteCreateResponseDto createDirectHubRoute(Long userId, UUID fromHubId, UUID toHubId) {
+        Hub fromHub = hubRepository.findById(fromHubId).orElseThrow(ResourceNotFoundException::new);
+        Hub toHub = hubRepository.findById(toHubId).orElseThrow(ResourceNotFoundException::new);
 
-        HubRoute saveHubRoute = HubRoute.builder()
-            .fromHub(fromHub)
-            .toHub(toHub)
-            .userId(userId)
-            .build();
+        HubRoute saveHubRoute = new HubRoute(fromHub, toHub, userId);
 
-        HubRoute hubRoute = hubRouteRepository.save(saveHubRoute)
-            .orElseThrow(ResourceNotFoundException::new);
-        return new HubRouteCreateResponse(hubRoute);
+        HubRoute hubRoute = hubRouteRepository.save(saveHubRoute);
+        return new HubRouteCreateResponseDto(hubRoute);
     }
 
     // 허브 간 경로 정보 삭제 (soft deleted)
     @Transactional
-    public HubRouteDeleteResponse deleteHubRoute(UUID hubRouteId, Long userId) {
+    public HubRouteDeleteResponseDto deleteHubRoute(UUID hubRouteId, Long userId) {
         HubRoute hubRoute = hubRouteRepository.findByHubRouteIdAndIsDeletedFalse(hubRouteId)
             .orElseThrow(ResourceNotFoundException::new);
 
@@ -82,7 +85,41 @@ public class HubRouteService {
                 .forEach(checkpoint -> checkpoint.delete(userId));
 
         hubRoute.delete(userId);
-        return new HubRouteDeleteResponse(hubRouteId, "Hub successfully deleted.");
+        return new HubRouteDeleteResponseDto(hubRouteId, "Hub successfully deleted.");
     }
+
+    // fromHub -> toHub 최단 경로 생성
+    @Transactional
+    public HubRouteDetailsResponseDto createPathHubRoute(UUID fromHubId, UUID toHubId, Long userId) {
+        Hub fromHub = hubRepository.findById(fromHubId).orElseThrow(ResourceNotFoundException::new);
+        Hub toHub = hubRepository.findById(toHubId).orElseThrow(ResourceNotFoundException::new);
+
+        List<Hub> shortPath = pathCalculate.getShortPath(fromHub, toHub, userId);
+        PathValueDto vlaues = pathCalculate.getValue(shortPath);
+
+        if(hubRouteRepository.findByFromHubAndToHub(fromHub, toHub).isPresent()){
+            throw new EntityExistsException("Hub route already exists");
+        }
+
+        HubRoute route = new HubRoute(fromHub, toHub, vlaues.getTotalDuration(), vlaues.getTotalDistance(), userId);
+        route = hubRouteRepository.save(route);
+
+        List<HubRouteCheckpoint> checkpointList = new ArrayList<>();
+        for(int i = 0; i < shortPath.size(); i++) {
+            HubRouteCheckpoint result = HubRouteCheckpoint.builder()
+                .checkpointHub(shortPath.get(i))
+                .hubRoute(route)
+                .userId(userId)
+                .sequence(i)
+                .build();
+            checkpointList.add(result);
+        }
+        checkpointRepository.saveAll(checkpointList);
+
+        return new HubRouteDetailsResponseDto(route, checkpointList);
+    }
+
+    // fromHub -> toHub 최단 경로 시퀀스 조회
+
 
 }
