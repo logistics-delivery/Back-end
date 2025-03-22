1611e3511bbb08327dce63ebc512241f9227e943
devdaeun <ekdms008@gmail.com>
2025-03-22 17:06:26 +0900

    ✨ feat [#134] : 공통모듈 AOP 작성 - AOP 적용을위해 각 서브모듈의 Application에 설정을 추가했습니다. - 공통모듈에 설정된 AOP 를 빈으로 등록시키기위한 설정입니다.

--- user-service/src/main/java/com/sparta/user/infastructure/repository/JpaUserRepository.java
+++ user-service/src/main/java/com/sparta/user/infastructure/repository/JpaUserRepository.java
@@ -3,11 +3,7 @@ package com.sparta.user.infastructure.repository;
 import com.sparta.user.domain.model.User;
 import com.sparta.user.domain.repository.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface JpaUserRepository extends JpaRepository<User, Long> , UserRepository {
