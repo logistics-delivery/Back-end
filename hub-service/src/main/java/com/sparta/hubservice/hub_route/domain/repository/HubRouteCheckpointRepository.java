c934c3d4f4f5e2f255cd446f8a820c256f98fd85
sohyun119 <sh-119@naver.com>
2025-03-22 22:40:14 +0900

    ✨ feat : Repository saveAll 메서드 추가

--- hub-service/src/main/java/com/sparta/hubservice/hub_route/domain/repository/HubRouteCheckpointRepository.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub_route/domain/repository/HubRouteCheckpointRepository.java
@@ -7,4 +7,6 @@ import java.util.UUID;
 public interface HubRouteCheckpointRepository {
 
     List<HubRouteCheckpoint> findAllByHubRouteId(UUID hubRouteId);
+
+    void saveAll(List<HubRouteCheckpoint> checkpointList);
 }
