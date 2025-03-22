9be04de1c858aad3440da49458e829adef9630b8
sohyun119 <sh-119@naver.com>
2025-03-22 22:32:14 +0900

    ✨ feat : from->to 최단 경로 생성 (path) Controller 구현

--- hub-service/src/main/java/com/sparta/hubservice/hub_route/presentation/controller/HubRouteController.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub_route/presentation/controller/HubRouteController.java
@@ -1,9 +1,10 @@
 package com.sparta.hubservice.hub_route.presentation.controller;
 
 
-import com.sparta.hubservice.hub_route.application.dto.response.HubRouteCreateResponse;
-import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDeleteResponse;
-import com.sparta.hubservice.hub_route.application.dto.response.HubRouteResponse;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteCreateResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDeleteResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDetailsResponseDto;
+import com.sparta.hubservice.hub_route.application.dto.response.HubRouteResponseDto;
 import com.sparta.hubservice.hub_route.application.service.HubRouteService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -28,47 +29,60 @@ public class HubRouteController {
 
     // 전체 허브 간 경로 목록 조회
     @GetMapping
-    public ResponseEntity<Page<HubRouteResponse>> getHubRoutes(
+    public ResponseEntity<Page<HubRouteResponseDto>> getHubRoutes(
         @PageableDefault(page = 0, size = 10, sort = "createdAt") Pageable pageable){
-        Page<HubRouteResponse> responses = hubRouteService.getHubRoutes(pageable);
+        Page<HubRouteResponseDto> responses = hubRouteService.getHubRoutes(pageable);
         return ResponseEntity.ok(responses);
     }
 
     // 특정 경로 ID 조회
     @GetMapping("/{hub_route_id}")
-    public ResponseEntity<HubRouteResponse> getHubRoute(@PathVariable("hub_route_id") UUID hubRouteId) {
-        HubRouteResponse response = hubRouteService.getHubRoute(hubRouteId);
+    public ResponseEntity<HubRouteResponseDto> getHubRoute(@PathVariable("hub_route_id") UUID hubRouteId) {
+        HubRouteResponseDto response = hubRouteService.getHubRoute(hubRouteId);
         return ResponseEntity.ok(response);
     }
 
-    // 특정 출발 허브 → 도착 허브 경로 조회
+    // 특정 출발 허브 → 도착 허브 경로 조회 (direct)
     @GetMapping("/{from_hub_id}/{to_hub_id}")
-    public ResponseEntity<HubRouteResponse> getHubRoute(
+    public ResponseEntity<HubRouteResponseDto> getDirectHubRoute(
         @PathVariable("from_hub_id") String fromHubId,
         @PathVariable("to_hub_id") String toHubId) {
-        HubRouteResponse response =  hubRouteService.getHubRouteFromHubToHub(UUID.fromString(fromHubId), UUID.fromString(toHubId));
+        HubRouteResponseDto response =  hubRouteService.getDirectHubRoute(UUID.fromString(fromHubId), UUID.fromString(toHubId));
         return ResponseEntity.ok(response);
     }
 
-    // (정해진) 허브 간 경로 생성
-    // Todo : 더미데이터 필요 (추후 중요한 경로만 주기적으로 naver api 실시간 거리 및 시간 반영 고려)
+    // (정해진) 허브 간 경로 생성 (direct)
+    // Todo : 더미데이터 필요
     @PostMapping("/{from_hub_id}/{to_hub_id}")
-    public ResponseEntity<HubRouteCreateResponse> createHubRoute(
+    public ResponseEntity<HubRouteCreateResponseDto> createDirectHubRoute(
         @PathVariable("from_hub_id") String fromHubId,
         @PathVariable("to_hub_id") String toHubId,
         @RequestParam Long userId) {
-        HubRouteCreateResponse response = hubRouteService.createHubRoute(userId, UUID.fromString(fromHubId), UUID.fromString(toHubId));
+        HubRouteCreateResponseDto response = hubRouteService.createDirectHubRoute(userId, UUID.fromString(fromHubId), UUID.fromString(toHubId));
         return ResponseEntity.ok(response);
     }
 
     // 허브 간 경로 정보 삭제
     @DeleteMapping("/{hub_route_id}")
-    public ResponseEntity<HubRouteDeleteResponse> deleteHubRoute(
+    public ResponseEntity<HubRouteDeleteResponseDto> deleteHubRoute(
         @PathVariable("hub_route_id") String hubRouteId,
         @RequestParam Long userId) {
-        HubRouteDeleteResponse response = hubRouteService.deleteHubRoute(UUID.fromString(hubRouteId), userId);
+        HubRouteDeleteResponseDto response = hubRouteService.deleteHubRoute(UUID.fromString(hubRouteId), userId);
         return ResponseEntity.ok(response);
     }
+
+    // form -> to 최단경로 생성
+    @PostMapping("/{from_hub_id}/{to_hub_id}")
+    public ResponseEntity<HubRouteDetailsResponseDto> createPathHubRoute(
+        @PathVariable("from_hub_id") String fromHubId,
+        @PathVariable("to_hub_id") String toHubId,
+        @RequestParam Long userId
+    ){
+        HubRouteDetailsResponseDto response = hubRouteService.createPathHubRoute(UUID.fromString(fromHubId), UUID.fromString(toHubId), userId);
+        return ResponseEntity.ok(response);
+    }
+
+    // fromHub -> toHub 최단 경로 시퀀스 조회
 
 
 }
