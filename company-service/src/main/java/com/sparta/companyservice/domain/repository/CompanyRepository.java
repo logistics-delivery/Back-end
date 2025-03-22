bc3d0a00485b1cc360dd3562532890373c2dd7e4
jiyun <jiyyy0720@naver.com>
2025-03-22 23:07:19 +0900

    🩹 fix [#114] : 리포지토리 수정

--- company-service/src/main/java/com/sparta/companyservice/domain/repository/CompanyRepository.java
+++ company-service/src/main/java/com/sparta/companyservice/domain/repository/CompanyRepository.java
@@ -1,6 +1,9 @@
 package com.sparta.companyservice.domain.repository;
 
 import com.sparta.companyservice.domain.model.Company;
+import com.sparta.companyservice.domain.model.CompanyType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +12,6 @@ public interface CompanyRepository {
     Company save(Company company);
 
     Optional<Company> findByIdAndDeletedAtIsNull(UUID id);
+
+    Page<Company> searchCompanies(String name, String address, CompanyType type, Pageable pageable);
 }
