549e35bd5dafa44b727893af283b3b22b0ee8da5
jiyun <jiyyy0720@naver.com>
2025-03-22 23:10:01 +0900

    🩹 fix [#114] : 리포지토리 수정2

--- company-service/src/main/java/com/sparta/companyservice/infrastructure/repository/JpaCompanyRepository.java
+++ company-service/src/main/java/com/sparta/companyservice/infrastructure/repository/JpaCompanyRepository.java
@@ -1,7 +1,6 @@
 package com.sparta.companyservice.infrastructure.repository;
 
 import com.sparta.companyservice.domain.model.Company;
-import com.sparta.companyservice.domain.repository.CompanyRepository;
 import com.sparta.companyservice.infrastructure.querydsl.CompanyQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface JpaCompanyRepository extends JpaRepository<Company, UUID>, CompanyRepository, CompanyQueryRepository {
+public interface JpaCompanyRepository extends JpaRepository<Company, UUID>, CompanyQueryRepository {
 }
