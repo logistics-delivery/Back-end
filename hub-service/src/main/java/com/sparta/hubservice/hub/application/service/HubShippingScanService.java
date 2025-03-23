68ba4e4a67e24b4dca62371090643e625a922699
sohyun119 <sh-119@naver.com>
2025-03-24 00:01:38 +0900

    ✨ feat : 출고처리 Service 코드 구현

--- hub-service/src/main/java/com/sparta/hubservice/hub/application/service/HubShippingScanService.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub/application/service/HubShippingScanService.java
@@ -7,6 +7,7 @@ import com.sparta.hubservice.hub.domain.model.HubShippingScanLog;
 import com.sparta.hubservice.hub.domain.repository.HubRepository;
 import com.sparta.hubservice.hub.domain.repository.HubShippingScanRepository;
 import com.sparta.hubservice.hub.infrastructure.feignclient.dto.InboundStatusResponseDto;
+import com.sparta.hubservice.hub.infrastructure.feignclient.dto.OutboundStatusResponseDto;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,9 +31,22 @@ public class HubShippingScanService {
             HubShippingScanLog.createInboundLog(hub, shippingId, userId);
 
         hubShippingScanRepository.save(hubShippingScanLog);
-        log.info("Success save hubShippingScanLog - HubShippingScanLog: {}", hubShippingScanLog);
+        log.info("Success Save InboundLog - HubShippingScanLog: {}", hubShippingScanLog);
 
         return shippingService.inboundStatus(hubShippingScanLog);
     }
 
+    @Transactional
+    public OutboundStatusResponseDto createOutbound(UUID hubId, UUID shippingId, UUID nextHubId, Long userId) {
+        Hub hub = hubRepository.findById(hubId).orElseThrow(ResourceNotFoundException::new);
+        Hub nextHub = hubRepository.findById(nextHubId).orElseThrow(ResourceNotFoundException::new);
+
+        HubShippingScanLog hubShippingScanLog =
+            HubShippingScanLog.createOutboundLog(hub, shippingId, nextHub, userId);
+
+        hubShippingScanRepository.save(hubShippingScanLog);
+        log.info("Success Save OutboundLog - HubShippingScanLog: {}", hubShippingScanLog);
+
+        return shippingService.outboundStatus(hubShippingScanLog);
+    }
 }
