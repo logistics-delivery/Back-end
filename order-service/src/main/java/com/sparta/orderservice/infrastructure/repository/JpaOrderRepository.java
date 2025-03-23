cd43fabd7a1e5e808b25757a34443362e11c2c2f
dydwo6018 <dydwo6018@gmail.com>
2025-03-23 17:49:58 +0900

    ✨ feat : OrderQueryDSLRepository 상속

--- order-service/src/main/java/com/sparta/orderservice/infrastructure/repository/JpaOrderRepository.java
+++ order-service/src/main/java/com/sparta/orderservice/infrastructure/repository/JpaOrderRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.orderservice.infrastructure.repository;
 
 import com.sparta.orderservice.domain.model.Order;
+import com.sparta.orderservice.domain.repository.OrderQueryDSLRepository;
 import com.sparta.orderservice.domain.repository.OrderRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID> {
+public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID>, OrderQueryDSLRepository {
 
 }
