bf1a09bcc82429ef746918554524091e88262b21
sohyun119 <sh-119@naver.com>
2025-03-24 02:55:20 +0900

    🩹 fix : BaseEntity 에서 @~Date 관련 어노테이션 작동할 수 있게 수정 - JpaAuditingConfig 파일을 통해 어노테이션 활성화 - AuditorAwareImpl.java 파일은 주석 처리   ㄴ 모든 모듈 시큐리티 적용시에만 사용 가능 (현재는 주석 풀면 에러)   ㄴ 사용시 생성자, update 메서드 필요없이 자동으로 ~by 값 주입 가능

--- common-module/src/main/java/com/sparta/commonmodule/entity/BaseEntity.java
+++ common-module/src/main/java/com/sparta/commonmodule/entity/BaseEntity.java
@@ -2,11 +2,8 @@ package com.sparta.commonmodule.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
-
 import java.time.LocalDateTime;
-
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -33,11 +30,11 @@ public class BaseEntity {
     private Long createdBy;
 
     @LastModifiedDate
-    @Column(name = "updated_at", nullable = true)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
-    @Column(name = "updated_by", nullable = true)
+    @Column(name = "updated_by")
     private Long updatedBy;
 
 
