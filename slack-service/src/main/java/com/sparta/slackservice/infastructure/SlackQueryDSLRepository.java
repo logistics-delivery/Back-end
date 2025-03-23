cd7288411a93342113c0f64c5c7860b4b06dd04c
devdaeun <ekdms008@gmail.com>
2025-03-23 18:47:05 +0900

    ✨ feat [#158] : Slack search추가 - querydsl활용한 검색기능 구현

--- slack-service/src/main/java/com/sparta/slackservice/infastructure/SlackQueryDSLRepository.java
+++ slack-service/src/main/java/com/sparta/slackservice/infastructure/SlackQueryDSLRepository.java
@@ -1,14 +1,11 @@
-package com.sparta.slackservice.domain.repository;
+package com.sparta.slackservice.infastructure;
 
+import com.sparta.slackservice.application.dto.SlackRequestDto;
 import com.sparta.slackservice.application.dto.SlackResponseDto;
 import com.sparta.slackservice.application.dto.SlackSearchRequestDto;
-import com.sparta.slackservice.domain.model.Slack;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
-public interface SlackRepository {
+public interface SlackQueryDSLRepository {
     Page<SlackResponseDto> searchSlack(SlackSearchRequestDto requestDto, Pageable pageable);
-
 }
