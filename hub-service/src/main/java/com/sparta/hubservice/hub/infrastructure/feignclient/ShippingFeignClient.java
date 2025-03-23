ca3cc11302938461838bdc6279dc8ef407cf414e
sohyun119 <sh-119@naver.com>
2025-03-23 18:36:26 +0900

    ✨ feat : Feign client 호출 클래스 작성

---
+++ hub-service/src/main/java/com/sparta/hubservice/hub/infrastructure/feignclient/ShippingFeignClient.java
@@ -0,0 +1,28 @@
+package com.sparta.hubservice.hub.infrastructure.feignclient;
+
+import com.sparta.hubservice.hub.infrastructure.feignclient.dto.InboundStatusRequestDto;
+import com.sparta.hubservice.hub.infrastructure.feignclient.dto.InboundStatusResponseDto;
+import com.sparta.hubservice.hub.infrastructure.feignclient.dto.OutboundStatusRequestDto;
+import com.sparta.hubservice.hub.infrastructure.feignclient.dto.OutboundStatusResponseDto;
+import java.util.UUID;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name ="shipping-service", url="${shipping.service.url}")
+public interface ShippingFeignClient {
+
+    // 입고 처리
+    @PostMapping("/api/v1/shippings/{shipping_id}/inbound")
+    InboundStatusResponseDto inboundStatus(
+        @PathVariable("shipping_id") UUID shippingId,
+        @RequestBody InboundStatusRequestDto inboundStatusRequestDto);
+
+    // 출고 처리
+    @PostMapping("/api/v1/shippings/{shipping_id}/outbound")
+    OutboundStatusResponseDto outboundStatus(
+        @PathVariable("shipping_id") UUID shippingId,
+        @RequestBody OutboundStatusRequestDto outboundStatusRequestDto);
+
+}
