3bee02a4771f99151d921019fcc7247364dfc489
jin2304 <search2304@naver.com>
2025-03-24 02:58:48 +0900

    ♻️ refactor [#139] : 상품 api 권한 추가

--- product-service/src/main/java/com/sparta/product/presentation/ProductController.java
+++ product-service/src/main/java/com/sparta/product/presentation/ProductController.java
@@ -34,6 +34,7 @@ public class ProductController {
      * 상품 생성
      */
     @Operation(summary = "Product 등록", description = "Product 생성 api 입니다.")
+    @RoleCheck("ROLE_MASTER, ROLE_HUB, ROLE_COMPANY")
     @PostMapping
     public ResponseEntity<CreateProductResponseDto> createProduct(@RequestBody CreateProductRequestDto requestDto,
                                                                   @RequestHeader(value = "user_id", required = true) Long userId) {
@@ -45,6 +46,7 @@ public class ProductController {
      * 상품 단일 조회
      */
     @Operation(summary = "Product 단일 조회", description = "Product 단일 조회 api 입니다.")
+    @RoleCheck("ROLE_MASTER, ROLE_HUB, ROLE_COMPANY, ROLE_SHIPPING")
     @GetMapping("/{productId}")
     public ResponseEntity<ReadProductResponseDto> readProduct(@PathVariable UUID productId) {
         return ResponseEntity.ok(productServiceImpl.readProduct(productId));
@@ -54,6 +56,7 @@ public class ProductController {
     /**
      * 상품 목록 조회
      */
+    @RoleCheck("ROLE_MASTER, ROLE_HUB, ROLE_COMPANY, ROLE_SHIPPING")
     @GetMapping
     public ResponseEntity<List<ReadProductResponseDto>> readAllProduct() {
         return ResponseEntity.ok(productServiceImpl.readAllProduct());
@@ -64,6 +67,7 @@ public class ProductController {
      * 상품 수정
      */
     @PutMapping("/{productId}")
+    @RoleCheck("ROLE_MASTER, ROLE_HUB, ROLE_COMPANY")
     public ResponseEntity<UpdateProductResponseDto> updateProduct(@PathVariable UUID productId,
                                                                   @RequestBody UpdateProductRequestDto requestDto) {
         return ResponseEntity.ok(productServiceImpl.updateProduct(
@@ -75,6 +79,7 @@ public class ProductController {
      * 상품 삭제
      */
     @DeleteMapping("/{productId}")
+    @RoleCheck("ROLE_MASTER, ROLE_HUB")
     public ResponseEntity<Void> deleteProduct(@PathVariable UUID productId,
                                               @RequestHeader(value = "user_id", required = true) Long userId) {
         productServiceImpl.deleteProduct(
@@ -87,6 +92,7 @@ public class ProductController {
      * 상품 검색
      */
     @GetMapping("/search")
+    @RoleCheck("ROLE_MASTER, ROLE_HUB, ROLE_COMPANY, ROLE_SHIPPING")
     public ResponseEntity<Page<SearchProductResponseDto>> searchProducts(@ModelAttribute SearchProductRequestDto requestDto,
                                                                          Pageable pageable) {
         return ResponseEntity.ok(productServiceImpl.searchProducts(requestDto, pageable));
