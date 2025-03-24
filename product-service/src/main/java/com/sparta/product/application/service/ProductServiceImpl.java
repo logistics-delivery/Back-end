3c8c60cdc8a3f6122f6150127568329e2cc17b13
jin2304 <search2304@naver.com>
2025-03-25 02:08:28 +0900

    ♻️ refactor [#191] : 상품에서 CompanyClient 수정

--- product-service/src/main/java/com/sparta/product/application/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/sparta/product/application/service/ProductServiceImpl.java
@@ -137,7 +137,7 @@ public class ProductServiceImpl implements ProductService {
 
     // 업체 존재 검증 메서드
     private void validateCompanyExists(UUID companyId) {
-        if (!companyClient.getCompanyById(companyId)) {
+        if (!companyClient.existsById(companyId)) {
             throw new ResourceNotFoundException("해당 업체가 존재하지 않습니다.");
         }
     }
