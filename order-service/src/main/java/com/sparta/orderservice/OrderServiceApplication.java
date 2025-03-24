e36dd3d0d6cbbabdb3564bd2fe1a4f7e7b42a27c
dydwo6018 <dydwo6018@gmail.com>
2025-03-24 21:12:31 +0900

    ✨ feat: JpaAuditingConfig.class 설정

--- order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
@@ -1,5 +1,6 @@
 package com.sparta.orderservice;
 
+import com.sparta.commonmodule.config.JpaAuditingConfig;
 import com.sparta.commonmodule.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableFeignClients(basePackages = "com.sparta.orderservice.infrastructure.client")
 @EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "com.sparta")
-@Import(SwaggerConfig.class)
+@Import({SwaggerConfig.class, JpaAuditingConfig.class})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
