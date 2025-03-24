574d4269fbf52bd9525fd731f039ecff4f6cea4e
dydwo6018 <dydwo6018@gmail.com>
2025-03-24 22:17:06 +0900

    🐛 bugfix: JpaAuditingConfig.class 설정 관련 필요없는 코드 삭제

--- order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients(basePackages = "com.sparta.orderservice.infrastructure.client")
-@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "com.sparta")
 @Import({SwaggerConfig.class, JpaAuditingConfig.class})
 public class OrderServiceApplication {
