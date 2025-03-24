7642abf34b664abdd5bf94ea9084989ec760c3c0
sohyun119 <sh-119@naver.com>
2025-03-24 16:16:35 +0900

    🩹 fix : @RoleChek 설정 및 @RequestHeader로 userId 받아올 수 있게 수정

--- hub-service/src/main/java/com/sparta/hubservice/hub_route/presentation/controller/HubRouteController.java
+++ hub-service/src/main/java/com/sparta/hubservice/hub_route/presentation/controller/HubRouteController.java
@@ -1,6 +1,7 @@
 package com.sparta.hubservice.hub_route.presentation.controller;
 
 
+import com.sparta.commonmodule.aop.RoleCheck;
 import com.sparta.hubservice.hub_route.application.dto.response.HubRouteCreateResponseDto;
 import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDeleteResponseDto;
 import com.sparta.hubservice.hub_route.application.dto.response.HubRouteDetailsResponseDto;
@@ -8,6 +9,7 @@ import com.sparta.hubservice.hub_route.application.dto.response.HubRouteResponse
 import com.sparta.hubservice.hub_route.application.service.HubRouteService;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,31 +55,34 @@ public class HubRouteController {
     }
 
     // (정해진) 허브 간 경로 생성 (direct)
+    @RoleCheck("ROLE_MASTER")
     @PostMapping("/{from_hub_id}/{to_hub_id}/direct")
     public ResponseEntity<HubRouteCreateResponseDto> createDirectHubRoute(
         @PathVariable("from_hub_id") UUID fromHubId,
         @PathVariable("to_hub_id") UUID toHubId,
-        @RequestParam Long userId) {
+        @RequestHeader("user_id") Long userId) {
         HubRouteCreateResponseDto response =
             hubRouteService.createDirectHubRoute(userId, fromHubId, toHubId);
         return ResponseEntity.ok(response);
     }
 
     // 허브 간 경로 정보 삭제
+    @RoleCheck("ROLE_MASTER")
     @DeleteMapping("/{hub_route_id}")
     public ResponseEntity<HubRouteDeleteResponseDto> deleteHubRoute(
         @PathVariable("hub_route_id") UUID hubRouteId,
-        @RequestParam Long userId) {
+        @RequestHeader("user_id") Long userId) {
         HubRouteDeleteResponseDto response = hubRouteService.deleteHubRoute(hubRouteId, userId);
         return ResponseEntity.ok(response);
     }
 
     // form -> to 최단경로 생성 (다이렉트는 항상 최단경로)
+    @RoleCheck("ROLE_MASTER")
     @PostMapping("/{from_hub_id}/{to_hub_id}/path")
     public ResponseEntity<HubRouteDetailsResponseDto> createPathHubRoute(
         @PathVariable("from_hub_id") UUID fromHubId,
         @PathVariable("to_hub_id") UUID toHubId,
-        @RequestParam Long userId
+        @RequestHeader("user_id") Long userId
     ){
         HubRouteDetailsResponseDto response =
             hubRouteService.createPathHubRoute(fromHubId, toHubId, userId);
