9f4142e4cdbbd170da752a731e3277d0abaf2868
jin2304 <search2304@naver.com>
2025-04-01 18:39:31 +0900

    ✨feat [#238] : stock-service init 추가

---
+++ stock-service/src/main/java/com/sparta/stockservice/presentation/StockController.java
@@ -0,0 +1,17 @@
+package com.sparta.stockservice.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/stocks")
+public class StockController {
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "test ok";
+    }
+
+}
